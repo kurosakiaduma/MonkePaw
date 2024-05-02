59c28ef6f5cb91470769054a00e4f6adc7dd34dc
Tevin Aduma <tevin74@live.com>
2024-05-02 12:28:13 +0300

    include context and global keywords to encapsulate scopes

--- Paw/tokens/tokens.py
+++ Paw/tokens/tokens.py
@@ -1,46 +1,50 @@
 from typing import Any, Literal
+
+
 class Token:
-    def __init__(self, type_:Literal["str"]|str, lexeme:str, begin_position:int|None = None, line_position:int|None = None, symbol_table_ref:Any|None = None):
+    def __init__(self, type_: Literal["str"] | str, lexeme: str, begin_position: int | None = None,
+                 line_position: int | None = None, symbol_table_ref: Any | None = None):
         self.type = type_
         self.lexeme = lexeme
         self.begin_position = begin_position
         self.line_position = line_position
         self.symbol_table_ref = symbol_table_ref
-    
+
     def __str__(self) -> str:
         return f'<Type:{self.type}, Lexeme:{self.lexeme}, BeginPosition:{self.begin_position}, LinePosition:{self.line_position}>'
 
     def __repr__(self) -> str:
         return f'<Type:{self.type}, Lexeme:{self.lexeme}, BeginPosition:{self.begin_position}, LinePosition:{self.line_position}>'
 
+
 # token types
 ILLEGAL = "ILLEGAL"
 EOF = "EOF"
 
 # Identifiers + lexemes
-IDENT = "IDENT" # add, x, any, ...
-INT = "INT"   # 1343456
-FLOAT = "FLOAT" # 12.23
-STR = "STR" # "I am a string"
-BOOL = "BOOL" # True or False i.e. Boolean
+IDENT = "IDENT"  # add, x, any, ...
+INT = "INT"  # 1343456
+FLOAT = "FLOAT"  # 12.23
+STR = "STR"  # "I am a string"
+BOOL = "BOOL"  # True or False i.e. Boolean
 
 # Operators
-ASSIGN   = "="
-PLUS     = "+"
-MINUS    = "-"
-BANG     = "!"
+ASSIGN = "="
+PLUS = "+"
+MINUS = "-"
+BANG = "!"
 ASTERISK = "*"
-SLASH    = "/"
-LT_EQ    = "<="
-LT       = "<"
-GT_EQ    = ">="
-GT       = ">"
-EQ       = "=="
-NOT_EQ   = "!="
-DOT      = "."
+SLASH = "/"
+LT_EQ = "<="
+LT = "<"
+GT_EQ = ">="
+GT = ">"
+EQ = "=="
+NOT_EQ = "!="
+DOT = "."
 
-#Delimiters
-COMMA     = ","
+# Delimiters
+COMMA = ","
 SEMICOLON = ";"
 
 LPAREN = "("
@@ -48,28 +52,30 @@ RPAREN = ")"
 LBRACE = "{"
 RBRACE = "}"
 
-#Keywords
+# Keywords
+CONTEXT = "CONTEXT"
+GLOBAL = "GLOBAL"
 FUNCTION = "FUNCTION"
-PRINT    = "PRINT"
-LET      = "LET"
-IF       = "IF"
-ELSE     = "ELSE"
-RETURN   = "RETURN"
-TRUE     = "TRUE"
-FALSE    = "FALSE"
-CLOCK    = "CLOCK"
-LEN      = "LEN"
-MATH     = "MATH"
+PRINT = "PRINT"
+LET = "LET"
+IF = "IF"
+ELSE = "ELSE"
+RETURN = "RETURN"
+TRUE = "TRUE"
+FALSE = "FALSE"
+CLOCK = "CLOCK"
+LEN = "LEN"
+MATH = "MATH"
 
 keywords = {
-    "fn":     FUNCTION,
-    "let":    LET,
-    "print":  PRINT,
-    "if":     IF,
-    "else":   ELSE,
+    "fn": FUNCTION,
+    "let": LET,
+    "print": PRINT,
+    "if": IF,
+    "else": ELSE,
     "return": RETURN,
-    "true":   TRUE,
-    "false":  FALSE,
+    "true": TRUE,
+    "false": FALSE,
     "int": INT,
     "str": STR,
     "float": FLOAT,
