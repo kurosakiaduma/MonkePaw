2c79fd9b7c5aa1c87061bbd1de11a00f85e31ccd
Tevin Aduma <tevin74@live.com>
2024-05-28 16:46:25 +0300

    feat:overwrite repr and str dunders for the ParseError class

--- Paw/parser/p_err.py
+++ Paw/parser/p_err.py
@@ -6,5 +6,12 @@ class ParseError:
                  token: Token,
                  message: str
                  ):
-        self.error_info = f"Unexpected token {token} with lexeme '{token.lexeme}' at position {token.begin_position} on line {token.line_position}."
+        self.error_info = f"Unexpected token {token} with lexeme '{token.lexeme}' at position {token.begin_position} " \
+                          f"on line {token.line_position}."
         self.message = ''.join(info + '\n' for info in [message, self.error_info])
+
+    def __str__(self):
+        return self.error_info
+
+    def __repr__(self):
+        return self.message
